cd4e6fbdeb8d7de15570c79f87ea9e7f34225c5d
Ankit gupta <gupta.ankit78611@gmail.com>
2021-09-26 00:31:23 +0530

    final commmit

--- public/script.js
+++ public/script.js
@@ -1,13 +1,30 @@
+
 let send = document.querySelector('#send');
 let message=document.querySelector('#type-message');
 let chatBackground=document.querySelector('.chat-background');
 
-let name = prompt("Enter your name ");
+let name = prompt('Enter your name:')
+if(name!=null){
+    socket.emit('user-joined',name);
+}
 
 send.addEventListener('click',function(){
-    if(message.value){
+    let messageValue=message.value;
+
+    socket.emit('send-chat',{message:messageValue,name:name});
+
+    apppendText(messageValue,name,"send");
+    message.value=""; 
+    
+})
+
+function apppendText(messageValue,name,flag){
+    if(messageValue){
         let sendMessage=document.createElement('div');
+        if(flag=="send")
         sendMessage.setAttribute('class','send-message');
+        else
+        sendMessage.setAttribute('class','received-message');
 
         let chatName=document.createElement('div');
         chatName.setAttribute('class','chat-name');
@@ -15,13 +32,13 @@ send.addEventListener('click',function(){
 
         let chatMessage=document.createElement('div');
         chatMessage.setAttribute('class','chat-message');
-        chatMessage.innerHTML=message.value;
+        chatMessage.innerHTML=messageValue;
 
         sendMessage.appendChild(chatName);
         sendMessage.appendChild(chatMessage);
         chatBackground.appendChild(sendMessage);
-        message.value=""; 
+        
         
         chatBackground.scrollTop = chatBackground.scrollHeight;
     }
-})
+}
